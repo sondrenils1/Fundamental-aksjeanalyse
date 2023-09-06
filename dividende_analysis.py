fba74c47fedaf19bf9a5e91db565c4b3ec4edd9c
sondrenils1 <116092604+sondrenils1@users.noreply.github.com>
2023-09-06 13:36:56 +0200

    Update dividende_analysis.py

--- dividende_analysis.py
+++ dividende_analysis.py
@@ -1,16 +1,15 @@
 import yfinance as yf
 import web_finans as fs
 import dfc_aker as dfc
-# Hent data for Apple fra Yahoo Finance
+
 
 tick = "AAPL"
 key = "Din API-key. Gå til FinancialModelingPrep.com"
 apple = yf.Ticker("AAPL")
 
-# Hent utbyttehistorikk
 utbytte_data = apple.dividends
 
-# Anta plowback-ratio og ROE
+
 Key_metrics = fs.get_key_metrics(ticker = tick, limit = 5, key = key)
 
 roe = Key_metrics.loc["roe"][0:5].mean()
@@ -28,11 +27,4 @@ vekstrate = dividende_yield * roe
 
 dividende_resultat = dividende_per_share / (coe-vekstrate)
 
-"""print(vekstrate)
-print(coe)
-print(dividende_per_share)
-print(dividende_yield)
-print(roe)"""
-
-#print(dividende_resultat)
      
